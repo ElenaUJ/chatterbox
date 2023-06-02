be4d15796a4803ecbdf341d26a884c254e244a0f
ElenaUJ <elena-ha@web.de>
2023-06-02 10:19:28 -0400

    Set up basic app architecure

---
+++ components/Start.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import {
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+const Start = () => {
+  return <View></View>;
+};
+
+export default Start;
