2a1f0be2528d7f8917a71b3da7b913b7d5a17cf3
ElenaUJ <elena-ha@web.de>
2023-06-06 11:08:56 -0400

    Implemented basic Gifted Chat interface

--- components/Chat.js
+++ components/Chat.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;
+
+  // State initializations
   const [backgroundColor, setBackgroundColor] = useState('#090C08');
+  const [messages, setMessages] = useState([]);
 
   const backgroundStyle = StyleSheet.create({
     background: {
@@ -16,9 +20,39 @@ const Chat = ({ route, navigation }) => {
   useEffect(() => {
     navigation.setOptions({ title: name });
     setBackgroundColor(color);
+    // Setting static message to be able to see changes in the UI
+    // Each message requires ID, creation date and user object as per Gifted Chat library
+    // User object requires ID, name, avatar
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
   }, []);
 
-  return <View style={backgroundStyle.background}></View>;
+  // setMessages() is called with callback function, that appends the new message to the previousMessages array, resulting in the newMessages array
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  return (
+    <View style={backgroundStyle.background}>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{ _id: 1 }}
+      />
+    </View>
+  );
 };
 
 export default Chat;
